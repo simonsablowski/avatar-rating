ecb72dedb54c93b5100f96adb7e1566247d17f8f
Simon Sablowski <contact@simsab.net>
2012-02-08 14:44:42 +0000

    - replacing images in config - changing texts - minor corrections

--- config.php
+++ config.php
@@ -1,25 +1,59 @@
 <?php
 
 $images = array(
-	1 => '1.jpg',
-	2 => '2.jpg',
-	3 => '3.jpg',
-	4 => '4.jpg',
-	5 => '5.jpg'
+	1 => 'cfAV1.png',
+	2 => 'cfAV2.png',
+	3 => 'cfAV3.png',
+	4 => 'cfAV4.png',
+	5 => 'cfAV5.png',
+	6 => 'cfAV6.png',
+	7 => 'cfAV7.png',
+	8 => 'cfAV8.png',
+	9 => 'cfAV9.png',
+	10 => 'cfAV10.png',
+	11 => 'cfAV11.png',
+	12 => 'cfAV12.png',
+	13 => 'cfAV13.png',
+	14 => 'cfAV14.png',
+	15 => 'cfAV15.png',
+	16 => 'cfAV16.png',
+	17 => 'cfAV17.png',
+	18 => 'cfAV18.png',
+	19 => 'cmAV1.png',
+	20 => 'cmAV2.png',
+	21 => 'cmAV3.png',
+	22 => 'cmAV4.png',
+	23 => 'cmAV5.png',
+	24 => 'cmAV6.png',
+	25 => 'cmAV7.png',
+	26 => 'cmAV8.png',
+	27 => 'cmAV9.png',
+	28 => 'cmAV10.png',
+	29 => 'cmAV11.png',
+	30 => 'cmAV12.png',
+	31 => 'cmAV13.png',
+	32 => 'cmAV14.png',
+	33 => 'cmAV15.png',
+	34 => 'cmAV16.png',
+	35 => 'cmAV17.png',
+	36 => 'cmAV18.png'
 );
 
-$options = array(
-	1 => 'sehr sympathisch',
-	2 => 'ziemlich sympathisch',
-	3 => 'sympathisch',
-	4 => 'weniger sympathisch',
-	5 => 'unsympathisch'
+$ratingOptions = array(
+	1 => 'very attractive',
+	2 => 'quite attractive',
+	3 => 'attractive',
+	4 => 'neutral',
+	5 => 'unattractive',
+	6 => 'quite unattractive',
+	7 => 'very unattractive'
 );
 
-$title = 'Sympathie von Avataren';
-$submit = 'Weiter';
-$thanks = 'Vielen Dank! Im Folgenden werden Ihre Ergebnisse noch einmal aufgelistet:';
-$stepFormat = 'Schritt %d von %d';
+$title = 'Online Avatar Attractiveness';
+$submitRating = 'Next';
+$stepFormat = 'Step %d/%d';
+$welcome = "Welcome!\nOn the following page you will be shown a series of images of avatars similar to the one below. We ask you to take a moment and tell us how attractive or unattractive you rate this avatar's appearance on a scale of 1 to 7 as shown below\nAfter your rating we would like to ask you a couple of questions about your rating.\nThank you for your participation!";
+$thanks = "Thank you for your help! If you don't mind, we have a few more questions before you go.";
 
 $basePath = '/avatars/';
 $dataFile = 'data.txt';

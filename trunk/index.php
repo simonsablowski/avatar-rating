c870c070cf6c9e3a28ba8ae69cd0c6e7d0a88a00
simbobwe@gmail.com <simbobwe@gmail.com@ba15cf18-c501-57d1-7a28-42b56f08e1ce>
2012-02-08 00:51:06 +0000

    initial commit

---
+++ trunk/index.php
@@ -0,0 +1,45 @@
+<?php
+
+include 'config.php';
+
+$done = false;
+$ratings = array();
+$results = array();
+$amountSteps = count($images);
+
+session_start();
+
+if (!isset($_SESSION['images'])) {
+	$shuffled = $images;
+	shuffle($shuffled);
+	$_SESSION['images'] = $shuffled;
+}
+
+if (!isset($_SESSION['ratings'])) {
+	$_SESSION['ratings'] = array();
+}
+
+if (isset($_POST['rating']) && isset($_POST['image'])) {
+	$_SESSION['ratings'][$_POST['image']] = $_POST['rating'];
+	array_pop($_SESSION['images']);
+}
+
+$image = end($_SESSION['images']);
+
+$step = min($amountSteps, count($_SESSION['ratings']) + 1);
+
+if (count($_SESSION['ratings']) == $amountSteps) {
+	$done = true;
+	
+	foreach ($_SESSION['ratings'] as $image => $rating) {
+		$results[array_search($image, $images)] = $rating;
+		$ratings[$image] = isset($options[$rating]) ? $options[$rating] : null;
+	}
+	
+	ksort($results);
+	$handle = fopen($dataFile, 'a');
+	fputcsv($handle, $results);
+	fclose($handle);
+}
+
+include 'layout.php';
